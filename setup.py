452b8bfb6c8edf61470e51c08889ea3f5fd3fb33
Avishay Bar <avishay.il@gmail.com>
2022-01-18 23:14:43 +0200

    fix: fixed long description for pypi release

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ setup(
     },
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + "\n\n" + history,
+    long_description=readme,
     include_package_data=True,
     keywords="secure_ec2",
     name="secure_ec2",
