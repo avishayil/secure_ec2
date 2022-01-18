925e5cd48588c1644d93a482480fd6d53e5da473
Al S <xee5ch.gh.al@il5.in>
2022-01-17 20:19:46 -0500

    Fix flake8 lints.

--- secure_ec2/src/aws.py
+++ secure_ec2/src/aws.py
@@ -43,9 +43,11 @@ def get_boto3_resource(
     resource = session.resource(service)
     return resource
 
+
 def is_regional_service(service: str):
     """Check if a service is in the availble service list via the API and see if it global."""
     return 'aws-global' not in boto3.session.Session().get_available_regions(service, allow_non_regional=True)
+
 
 def get_available_regions(service: str):
     """AWS exposes their list of regions as an API. Gather the list."""
